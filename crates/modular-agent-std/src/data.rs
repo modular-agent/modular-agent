9aabd6f0953ba689f2987c1360d42f0c7e9b9432
Akira Ishino <akira@lumilab.jp>
2025-11-24 17:40:20 +0900

    Get Value and Set Value

--- crates/modular-agent-std/src/data.rs
+++ crates/modular-agent-std/src/data.rs
@@ -88,13 +88,13 @@ impl AsAgent for FromJsonAgent {
     }
 }
 
-// Get Property
-struct GetPropertyAgent {
+// Get Value
+struct GetValueAgent {
     data: AsAgentData,
 }
 
 #[async_trait]
-impl AsAgent for GetPropertyAgent {
+impl AsAgent for GetValueAgent {
     fn new(
         askit: ASKit,
         id: String,
@@ -120,13 +120,13 @@ impl AsAgent for GetPropertyAgent {
         _pin: String,
         data: AgentData,
     ) -> Result<(), AgentError> {
-        let property = self.configs()?.get_string(CONFIG_PROPERTY)?;
+        let key = self.configs()?.get_string(CONFIG_KEY)?;
 
-        if property.is_empty() {
+        if key.is_empty() {
             return Ok(());
         }
 
-        let props = property.split('.').collect::<Vec<_>>();
+        let keys = key.split('.').collect::<Vec<_>>();
 
         if data.is_array() {
             let mut out_arr = Vec::new();
@@ -135,12 +135,12 @@ impl AsAgent for GetPropertyAgent {
                 .ok_or_else(|| AgentError::InvalidValue("failed as_array".to_string()))?
             {
                 let mut value = v.clone();
-                for prop in &props {
+                for key in &keys {
                     let Some(obj) = value.as_object() else {
                         value = AgentValue::unit();
                         break;
                     };
-                    if let Some(v) = obj.get(*prop) {
+                    if let Some(v) = obj.get(*key) {
                         value = v.clone();
                     } else {
                         value = AgentValue::unit();
@@ -154,15 +154,15 @@ impl AsAgent for GetPropertyAgent {
             } else {
                 &out_arr[0].kind()
             };
-            self.try_output(ctx, PIN_DATA, AgentData::array(kind.to_string(), out_arr))?;
+            self.try_output(ctx, PIN_VALUE, AgentData::array(kind.to_string(), out_arr))?;
         } else if data.is_object() {
             let mut value = data.value;
-            for prop in props {
+            for key in keys {
                 let Some(obj) = value.as_object() else {
                     value = AgentValue::unit();
                     break;
                 };
-                if let Some(v) = obj.get(prop) {
+                if let Some(v) = obj.get(key) {
                     value = v.clone();
                 } else {
                     // TODO: Add a config to determine whether to output unit
@@ -171,7 +171,107 @@ impl AsAgent for GetPropertyAgent {
                 }
             }
 
-            self.try_output(ctx, PIN_DATA, AgentData::from_value(value))?;
+            self.try_output(ctx, PIN_VALUE, AgentData::from_value(value))?;
+        }
+
+        Ok(())
+    }
+}
+
+// Set Value
+struct SetValueAgent {
+    data: AsAgentData,
+    input_data: Option<AgentData>,
+    input_value: Option<AgentValue>,
+    current_id: usize,
+}
+
+#[async_trait]
+impl AsAgent for SetValueAgent {
+    fn new(
+        askit: ASKit,
+        id: String,
+        def_name: String,
+        config: Option<AgentConfigs>,
+    ) -> Result<Self, AgentError> {
+        Ok(Self {
+            data: AsAgentData::new(askit, id, def_name, config),
+            input_data: None,
+            input_value: None,
+            current_id: 0,
+        })
+    }
+
+    fn data(&self) -> &AsAgentData {
+        &self.data
+    }
+
+    fn mut_data(&mut self) -> &mut AsAgentData {
+        &mut self.data
+    }
+
+    async fn process(
+        &mut self,
+        ctx: AgentContext,
+        pin: String,
+        data: AgentData,
+    ) -> Result<(), AgentError> {
+        // Reset input values if context ID changes
+        let ctx_id = ctx.id();
+        if ctx_id != self.current_id {
+            self.current_id = ctx_id;
+            self.input_data = None;
+            self.input_value = None;
+        }
+
+        // Store input data or value
+        if pin == PIN_DATA {
+            if data.is_object() {
+                self.input_data = Some(data);
+            }
+        } else if pin == PIN_VALUE {
+            self.input_value = Some(data.value);
+        }
+        if self.input_data.is_none() || self.input_value.is_none() {
+            return Ok(());
+        }
+
+        // parse key
+        let key = self.configs()?.get_string(CONFIG_KEY)?;
+        if key.is_empty() {
+            return Ok(());
+        }
+        let keys = key.split('.').collect::<Vec<_>>();
+
+        // set value
+        if self.input_data.as_ref().unwrap().is_object() {
+            let new_value = self.input_value.take().unwrap();
+            let value = self.input_data.take().unwrap().value;
+            let mut obj = value
+                .as_object()
+                .ok_or_else(|| AgentError::InvalidValue("failed as_object_mut".to_string()))?
+                .clone();
+            let last_key = keys.last().unwrap();
+            let mut current = &mut obj;
+            for key in &keys[..keys.len() - 1] {
+                if !current.contains_key(*key) {
+                    current.insert((*key).to_string(), AgentValue::object_default());
+                }
+                let next = current
+                    .get_mut(*key)
+                    .ok_or_else(|| AgentError::InvalidValue("failed get_mut".to_string()))?;
+                let next_obj = next
+                    .as_object_mut()
+                    .ok_or_else(|| AgentError::InvalidValue("failed as_object_mut".to_string()))?;
+                current = next_obj;
+            }
+            current.insert((*last_key).to_string(), new_value);
+
+            self.try_output(
+                ctx,
+                PIN_DATA,
+                AgentData::from_value(AgentValue::object(obj)),
+            )?;
         }
 
         Ok(())
@@ -183,14 +283,15 @@ static CATEGORY: &str = "Core/Data";
 
 static PIN_DATA: &str = "data";
 static PIN_JSON: &str = "json";
+static PIN_VALUE: &str = "value";
 
-static CONFIG_PROPERTY: &str = "property";
+static CONFIG_KEY: &str = "key";
 
 pub fn register_agents(askit: &ASKit) {
     askit.register_agent(
         AgentDefinition::new(
             AGENT_KIND,
-            "std_to_json",
+            "std_data_to_json",
             Some(new_agent_boxed::<ToJsonAgent>),
         )
         .title("To JSON")
@@ -202,7 +303,7 @@ pub fn register_agents(askit: &ASKit) {
     askit.register_agent(
         AgentDefinition::new(
             AGENT_KIND,
-            "std_from_json",
+            "std_data_from_json",
             Some(new_agent_boxed::<FromJsonAgent>),
         )
         .title("From JSON")
@@ -214,13 +315,26 @@ pub fn register_agents(askit: &ASKit) {
     askit.register_agent(
         AgentDefinition::new(
             AGENT_KIND,
-            "std_get_property",
-            Some(new_agent_boxed::<GetPropertyAgent>),
+            "std_data_get_value",
+            Some(new_agent_boxed::<GetValueAgent>),
         )
-        .title("Get Property")
+        .title("Get Value")
         .category(CATEGORY)
         .inputs(vec![PIN_DATA])
+        .outputs(vec![PIN_VALUE])
+        .string_config_default(CONFIG_KEY),
+    );
+
+    askit.register_agent(
+        AgentDefinition::new(
+            AGENT_KIND,
+            "std_data_set_value",
+            Some(new_agent_boxed::<SetValueAgent>),
+        )
+        .title("Set Value")
+        .category(CATEGORY)
+        .inputs(vec![PIN_DATA, PIN_VALUE])
         .outputs(vec![PIN_DATA])
-        .string_config_default(CONFIG_PROPERTY),
+        .string_config_default(CONFIG_KEY),
     );
 }
