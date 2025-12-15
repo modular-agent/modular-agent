8c1bf1c614ccc46eaf47d8b01d4c873bf0d1b0ff
Akira Ishino <akira@lumilab.jp>
2025-12-15 21:52:31 +0900

    Collect Agent

--- crates/modular-agent-std/src/array.rs
+++ crates/modular-agent-std/src/array.rs
@@ -5,14 +5,14 @@ use agent_stream_kit::{
 
 static CATEGORY: &str = "Std/Array";
 
-static PIN_IN: &str = "in";
-static PIN_OUT: &str = "out";
+static PIN_ARRAY: &str = "array";
+static PIN_VALUE: &str = "value";
 
 #[askit_agent(
     title = "Map",
     category = CATEGORY,
-    inputs = [PIN_IN],
-    outputs = [PIN_OUT],
+    inputs = [PIN_ARRAY],
+    outputs = [PIN_VALUE],
 )]
 struct MapAgent {
     data: AgentData,
@@ -40,7 +40,7 @@ impl AsAgent for MapAgent {
                 let c = ctx
                     .with_var("map_i".into(), AgentValue::integer(i as i64))
                     .with_var("map_n".into(), AgentValue::integer(n as i64));
-                self.try_output(c, PIN_OUT, item.clone())?;
+                self.try_output(c, PIN_VALUE, item.clone())?;
             }
         } else {
             return Err(AgentError::InvalidValue(
@@ -48,5 +48,69 @@ impl AsAgent for MapAgent {
             ));
         }
         Ok(())
+    }
+}
+
+/// Collects input values into an array and emits the array.
+#[askit_agent(
+    title = "Collect",
+    category = CATEGORY,
+    inputs = [PIN_VALUE],
+    outputs = [PIN_ARRAY],
+    string_config(name = "timeout", default = "10s")
+)]
+struct CollectAgent {
+    data: AgentData,
+    input_values: Vec<Option<AgentValue>>,
+    current_id: usize,
+}
+
+#[async_trait]
+impl AsAgent for CollectAgent {
+    fn new(askit: ASKit, id: String, spec: AgentSpec) -> Result<Self, AgentError> {
+        let data = AgentData::new(askit, id, spec);
+        Ok(Self {
+            data,
+            input_values: Vec::new(),
+            current_id: 0,
+        })
+    }
+
+    async fn process(
+        &mut self,
+        ctx: AgentContext,
+        _pin: String,
+        value: AgentValue,
+    ) -> Result<(), AgentError> {
+        let Some(map_i) = ctx.get_var("map_i").and_then(|v| v.as_i64()) else {
+            // no need to collect
+            return self.try_output(ctx, PIN_ARRAY, value);
+        };
+        let Some(map_n) = ctx.get_var("map_n").and_then(|v| v.as_i64()) else {
+            // no need to collect
+            return self.try_output(ctx, PIN_ARRAY, value);
+        };
+
+        // Reset input values if context ID changes
+        let ctx_id = ctx.id();
+        if ctx_id != self.current_id {
+            self.current_id = ctx_id;
+            self.input_values = vec![None; map_n as usize];
+        }
+
+        self.input_values[map_i as usize] = Some(value);
+
+        // Check if some input is still missing
+        if self.input_values.iter().any(|v| v.is_none()) {
+            return Ok(());
+        }
+
+        // All inputs are present, emit the array
+        let arr: Vec<AgentValue> = self
+            .input_values
+            .iter()
+            .map(|v| v.clone().unwrap())
+            .collect();
+        self.try_output(ctx, PIN_ARRAY, AgentValue::array(arr))
     }
 }
